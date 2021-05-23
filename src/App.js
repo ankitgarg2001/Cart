dcd75cd813133c2b37bb2d03c1bb26fedcb5310c
ankitgarg2001 <ankitgarg2001>
2021-05-23 05:57:41 +0530

    added total cart value

--- src/App.js
+++ src/App.js
@@ -10,21 +10,21 @@ class App extends React.Component {
           price: 99,
           title: 'Watch',
           qty: 1,
-          img: '',
+          img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2F0Y2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
           id: 1
         },
         {
           price: 999,
           title: 'MobilePhone',
           qty: 10,
-          img: '',
+          img: 'https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=316&q=80',
           id: 2
         },
         {
           price: 999,
           title: 'Laptop',
           qty: 4,
-          img: '',
+          img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
           id: 3
         }
       ]
@@ -67,6 +67,14 @@ cartCount=()=>{
   products.forEach((item)=>count+=item.qty);
   return count;
 }
+getCartTotal=()=>{
+  const{products}=this.state;
+  let total = 0;
+  products.forEach((item)=>
+    total+=(item.qty*item.price)
+  );
+  return total;
+}
   render(){
     const{products} = this.state;
     return (
@@ -80,6 +88,9 @@ cartCount=()=>{
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
+        <div style = {{padding: 10,fontSize: 20}}>
+          TOTAL VALUE: {this.getCartTotal()}
+        </div>
       </div>
     );
   }
